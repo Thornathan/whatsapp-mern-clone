2788596552fa62d2718a50ae663908972f3d9910
Jonathan <Jonathan@Admins-MacBook-Pro.local>
2020-12-18 19:56:42 -0800

    Created components folder and templates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Chat from "./components/Chat/Chat";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Sidebar />
+      <Chat />
     </div>
   );
 }
